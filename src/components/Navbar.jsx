c7741959027283f9168679c89e9289db2f54e9a9
Henry Narzary <narzaryhenry0@gmail.com>
2025-02-20 20:03:15 +0530

    reset

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,5 @@
-import { FaUser } from "react-icons/fa";
-
 const Navbar = () => {
-  return (
-    <div className="shadow-md sticky top-0 bg-white z-50">
-      <nav className="flex justify-between items-center py-6 max-w-7xl mx-auto px-4">
-        <a href="/" className="text-2xl font-bold">
-          XXYYZZZ
-        </a>
-        <div className="flex items-center space-x-4">
-          <FaUser className="text-2xl" />
-        </div>
-      </nav>
-    </div>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
